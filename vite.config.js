394df1078e433c3f3872ab6bf30515a29a7cf498
sozdatel3 <110739584+sozdatel3@users.noreply.github.com>
2026-02-03 19:12:50 +0300

    move simple page to public for static serving

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
   base: '/',
   server: {
     allowedHosts: true
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(process.cwd(), 'index.html'),
-        simple: resolve(process.cwd(), 'simple/index.html')
-      }
-    }
   }
 })
