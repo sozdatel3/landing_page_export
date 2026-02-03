7edde4f415c0464d3cd25417216070845d77c44a
sozdatel3 <110739584+sozdatel3@users.noreply.github.com>
2026-02-03 19:02:47 +0300

    simple

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/',
   server: {
     allowedHosts: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+        simple: resolve(process.cwd(), 'simple/index.html')
+      }
+    }
   }
 })
